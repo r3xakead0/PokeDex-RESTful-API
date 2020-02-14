22cbaf579830ab66883fb936f9d3ecb4ca7ca61b
Afu Tse <solapa@gmail.com>
2020-02-14 13:28:54 -0500

    new schema

---
+++ PokeDex/Models/Entities/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeDex.Models.Entities
+{
+    public class Pokemon
+    {
+        [Required]
+        public string Code { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type1 { get; set; }
+        [Required]
+        public string Type2 { get; set; }
+    }
+}
---
+++ PokeDex/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PokeDex.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace PokeDex
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(
+                config =>
+                    {
+                        //Configuring Content-Negotiation
+                        config.RespectBrowserAcceptHeader = true;
+                        config.ReturnHttpNotAcceptable = true;
+                    }
+                )
+                .AddXmlSerializerFormatters()
+                .AddXmlDataContractSerializerFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Registering Database Context
+            //var connection = @"Server=(localdb)\MSSQLLocalDB;Database=PokeDex;Trusted_Connection=True;ConnectRetryCount=0";
+            var connection = @"Data Source=PokeDex.db";
+
+            services.AddDbContext<PokemonDbContext>
+                (options => options.UseSqlite(connection));
+
+            // Register the Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "PokeDex API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PokeDex API V1");
+            });
+            app.UseMvc();
+        }
+    }
+}
